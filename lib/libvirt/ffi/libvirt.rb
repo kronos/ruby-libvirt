2d3ff62d1fd0f2097c6727ac3637327b23202e58
Ivan Samsonov <hronya@gmail.com>
2010-05-19 17:47:38 +0400

    Declare methods before using it

--- lib/libvirt/ffi/libvirt.rb
+++ lib/libvirt/ffi/libvirt.rb
@@ -1,10 +1,10 @@
 module FFI::Libvirt
-  extend FFI::Library
-  ffi_lib FFI::Libvirt::library_path
-
   def self.library_path
     'libvirt.so.0'
   end
+
+  extend FFI::Library
+  ffi_lib FFI::Libvirt::library_path
 
   #int	virGetVersion(unsigned long * libVer, const char * type, unsigned long * typeVer)
   attach_function :virGetVersion, [:pointer, :string, :pointer], :int
